da086bc13e3208634816825d8db8f8f8348e6d9c
shayan semnani <33687690+shayansemnani78@users.noreply.github.com>
2019-05-16 15:16:28 +0430

    variable

--- variables.py
+++ variables.py
@@ -1,2 +1,78 @@
-num = 85
-print(type(num))
+#num = 85
+#print(type(num))
+#print(num)
+#float_num = 20.1
+#print(type(float_num))
+#print(float_num)
+#example_str = ("happy birthday of 20yrs Ariyo!")
+#print(type(example_str))
+#print(example_str)
+#cmpnum = 10+4j
+#cmpnum2 = 20+2j
+#print(type(cmpnum))
+#print(type(cmpnum2))
+#cmpnum3 = cmpnum+cmpnum2
+#print(cmpnum)
+#print(cmpnum2)
+#print(cmpnum3)
+#print(type(cmpnum3))
+#maghz_amirhossein=None
+#print(type(maghz_amirhossein))
+#print(maghz_amirhossein)
+#num=12
+#print(type(num))
+#print(num)
+#float_num=8.9
+#print(type(float_num))
+#print(float_num)
+#exmple_star ="shayan the real name is alfie the alpaca"
+##print(exmple_star)
+#comp_num=8-5j
+#comp_num2=4+3j
+#comp_num3=4+8j
+#comp_num4=comp_num+comp_num2-comp_num3
+#print(comp_num4)
+#print(type(comp_num4))
+##ten_percent_of_shayan_is_not_human=None
+#print(type(ten_percent_of_shayan_is_not_human))
+#print(ten_percent_of_shayan_is_not_human)
+
+
+
+
+tex="ma homie shayan sit here"
+print(tex[-8])
+print(tex[2:9])
+print(tex[5:85:3])
+mylist=[1, 7, 8, 85, 5]
+print(mylist)
+print(type(mylist))
+
+
+new_list=[1, 2.01, True, 4, "Hello!", 10000, 9999999]
+len_of_list=len(new_list)
+print(type(new_list))
+print(len_of_list)
+print(type(len_of_list))
+
+
+
+
+my_tuple=(85, 8, 5, True, 0.85)
+print(type(my_tuple))
+print(my_tuple)
+print(my_tuple[-1])
+
+
+
+band={"name":'pinkfloyd',
+      "members":"d.g & r.w & r.r",
+      "year":12,
+      "albums":"don't know"}
+print(type(band))
+print(band["name"])
+
+lst=[1, 2.01, True, 4, "Hello!", 10000, 9999999]
+print(lst[-1])
+print(lst[1:7:2])
+
