4fe4adcf45430a495586e7c08584c42c7f185f19
shayan semnani <33687690+shayansemnani78@users.noreply.github.com>
2019-05-17 11:50:35 +0430

    body_mass_index

--- method/method_test.py
+++ method/method_test.py
@@ -1,14 +1,14 @@
-weight=85
-height=1.85
-body_mass_index=weight/(height**2)
+weight = 85
+height = 1
+body_mass_index = weight/(height**2)
 
 print(body_mass_index)
 if body_mass_index < 20:
     print("low weight")
 
-if 20 < body_mass_index < 25:
+elif 20 < body_mass_index < 25:
     print("normal")
 
-if body_mass_index > 25:
+else:
     print("over weight")
 
