afa495fddd85c44b8e26985bea1fdd784342ecca
shayan semnani <33687690+shayansemnani78@users.noreply.github.com>
2019-05-23 16:36:38 +0430

    home_work_3

--- home_works/home_work_3.py
+++ home_works/home_work_3.py
@@ -7,11 +7,27 @@ middle=number_of_mems/2
 counter=0
 while counter < middle:
     lst_l.insert(counter,lst[counter])
-    print(lst_l)
     counter+=1
 
 counter_2=int(middle)
 while counter_2 < number_of_mems:
-    lst_r.insert(counter_2,lst[middle+1])
+
+    lst_r.insert(counter_2,lst[counter_2])
+    counter_2+=1
+
+position_choice=input("pls choose your position with R for right and L for left!!! ")
+
+if position_choice=="R" :
     print(lst_r)
-    middle+=1
+    lst_r.reverse()
+    print(lst_r)
+
+
+elif position_choice=="L" :
+    print(lst_l)
+    lst_l.reverse()
+    print(lst_l)
+
+else :
+    print("invalid position")
+
