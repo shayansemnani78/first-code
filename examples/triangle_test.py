6367d1fb88d39aec0e70652035fb5335eda06597
shayan semnani <33687690+shayansemnani78@users.noreply.github.com>
2019-06-21 10:18:04 +0430

    triangle_test

--- examples/triangle_test.py
+++ examples/triangle_test.py
@@ -1,13 +1,26 @@
 def triangle (h1, h2, h3):
-
-    if h1+h2 <= h3 :
-        return False
-    elif h1+h3 <= h2 :
-        return False
-    elif h2+h3 <= h1 :
-        return False
-    else :
-        return True
+     if h1+h2 <= h3:
+         return False
+     elif h1+h3 <= h2:
+         return False
+     elif h2+h3 <= h1:
+         return False
+     else :
+         return True
 
 a= triangle(1, 1, 2)
 print(a)
+
+
+
+def triangle (h1, h2, h3):
+
+    if h1+h2 <= h3 or h1+h3 <= h2 or h2+h3 <= h1:
+        return False
+
+    else :
+        return True
+
+a= triangle(10, 1, 1)
+print(a)
+#first code is faster
