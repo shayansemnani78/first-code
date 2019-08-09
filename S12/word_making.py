e677c2bb460768a2987295df93b970f3db03a2d8
Student <shayansemnani78@gmail.com>
2019-08-09 10:51:52 +0330

    alphabets

---
+++ S12/word_making.py
@@ -0,0 +1,12 @@
+# def alpha_list (in_text : str):
+#     pre_lst = []
+#     for i in in_text :
+#         pre_lst.append(i)
+#     list_len = pre_lst.__len__()
+#     return pre_lst , list_len
+#
+#
+# def alpha_game (input_lst : list, num : int):
+#     pass
+
+
